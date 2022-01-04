c9473b2067890d9948fc25cf78ddc5147a6464a5
Nina <florinaseipp@gmail.com>
2022-01-04 19:44:57 +0100

    added Axios and dictionary api
    documentation for dictionary api:
    https://dictionaryapi.dev/

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
+
+import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for the definition of ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
@@ -15,7 +23,12 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <form className="searchForm" onSubmit={search}>
-        <input type="search" autoFocus={true} onChange={handleKeywordChange} />
+        <input
+          type="search"
+          autoFocus={true}
+          onChange={handleKeywordChange}
+          className="input"
+        />
       </form>
     </div>
   );
