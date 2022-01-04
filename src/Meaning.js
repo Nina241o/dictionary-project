f7f44bbeaadb56075ad7b019890daba74243f58a
Nina <florinaseipp@gmail.com>
2022-01-04 21:58:09 +0100

    Changed code back to only show one definition of a word

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,18 +4,12 @@ export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
+      <br />
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
-            </p>
-          </div>
-        );
-      })}
+      <div>{props.meaning.definitions[0].definition}</div>
+      <p className="mt-4">
+        <em>{props.meaning.definitions[0].example}</em>
+      </p>
     </div>
   );
 }
