9f8b636fd5ef5244d84e99291aff6fa60e17836d
Nina <florinaseipp@gmail.com>
2022-01-05 19:30:59 +0100

    Built synonym function and styled it

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,15 +1,29 @@
 import React from "react";
 
+import Synonyms from "./Synonyms";
+
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      <br />
-      <h3>{props.meaning.partOfSpeech}</h3>
-      <div>{props.meaning.definitions[0].definition}</div>
-      <p className="mt-4">
-        <em>{props.meaning.definitions[0].example}</em>
-      </p>
+      <h4 className="mt-4 mb-3">{props.meaning.partOfSpeech}</h4>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p className="Definition">
+              <strong>Definition: </strong>
+              {definition.definition}
+              <br />
+              <div className="Example mt-2">
+                <strong>Example: </strong>
+                <em>{definition.example}</em>
+              </div>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-import Meaning from "./Meaning";
+import Meaning from "./Meaning"; 
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <h1>{props.results.word}</h1>
+        <h1 className="mb-4"><strong>{props.results.word}</strong></h1>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
